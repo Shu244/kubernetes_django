fed840959b2a0466ebc0755641fa0019da6cb365
Shuhao Lai <shuhaolai18@gmail.com>
2023-06-25 20:02:51 -0400

    notes

---
+++ deploy-all.sh
@@ -0,0 +1,6 @@
+kubectl apply -f deploy/kubernetes/rds/
+kubectl apply -f deploy/kubernetes/redis/
+kubectl apply -f deploy/kubernetes/django/
+kubectl apply -f deploy/kubernetes/celery/
+kubectl apply -f deploy/kubernetes/flower/
+kubectl apply -f deploy/kubernetes/test-deploy
